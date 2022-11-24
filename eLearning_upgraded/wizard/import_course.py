2eec8078fa065d85ff5ad564e48c14ad0166eca7
NicolasHerreraHdez <a01114972@itesm.mx>
2022-11-24 16:47:40 -0700

    Changed API link for import course request (Heroku)

--- eLearning_upgraded/wizard/import_course.py
+++ eLearning_upgraded/wizard/import_course.py
@@ -10,7 +10,7 @@ class ImportCourseWizard(models.TransientModel):
         if 'docs.google.com' in self.document_id and self.document_id.split('/')[-2]:
             doc_id = self.document_id.split('/')[-2]
             try:
-                r = requests.get(f'https://docs-to-json-wl.onrender.com/course_import/{doc_id}')
+                r = requests.get(f'https://docs-to-json-wl.herokuapp.com/course_import/{doc_id}')
                 return {
                     'type': 'ir.actions.client',
                     'tag': 'reload'
