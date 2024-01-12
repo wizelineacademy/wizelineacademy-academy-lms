bb1e8380cb095fcc88cf7b731ccdbd99dc08bda8
Nat <natalia.reyes@dowjones.com>
2024-01-12 15:57:14 -0500

    Added academy-theme first test

---
+++ academy-theme/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Academy Theme',
+    'description': 'Academy website theme',
+    'category': 'Theme',
+    'sequence': 10,
+    'version': '1.0',
+    'depends': ['website'],
+    'data': ['views/header.xml', 'views/footer.xml'],
+    'images': [
+    ],
+    'snippet_lists': {
+    },
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+
+}
