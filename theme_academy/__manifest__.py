66958a8017921175772a63980ad02e0b3f355366
Nat <natalia.reyes@dowjones.com>
2024-01-25 21:03:07 -0500

    remove comma

--- theme_academy/__manifest__.py
+++ theme_academy/__manifest__.py
@@ -31,7 +31,7 @@
         ],
         'web.assets_frontend': [
             "theme_academy/static/src/scss/header.scss",
-            "theme_academy/static/src/scss/footer.scss",
+            "theme_academy/static/src/scss/footer.scss"
         ],
     },
 }
